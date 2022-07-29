39e6c56d2fb2cf128f3ac6c4ae8c22d8e7793650
Antoine CORDIER <antoine.cordier@graviteesource.com>
2022-07-29 07:41:16 +0200

    fix: panic on nil context

--- controllers/internal/delegates/context/get.go
+++ controllers/internal/delegates/context/get.go
@@ -1,15 +1,24 @@
 package context
 
 import (
-	"k8s.io/apimachinery/pkg/types"
-
 	gio "github.com/gravitee-io/gravitee-kubernetes-operator/api/v1alpha1"
+	"github.com/gravitee-io/gravitee-kubernetes-operator/pkg/keys"
+	kerrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/types"
 )
 
 func (d *Delegate) Get(
 	api *gio.ApiDefinition,
 ) (*gio.ManagementContext, error) {
 	contextRef := api.Spec.Context
+
+	if contextRef == nil {
+		group := gio.GroupVersion.Group
+		resource := keys.CrdManagementContextResource
+		ref := schema.GroupResource{Group: group, Resource: resource}
+		return nil, kerrors.NewNotFound(ref, "")
+	}
 
 	apimContext := new(gio.ManagementContext)
 	ns := types.NamespacedName{Name: contextRef.Name, Namespace: contextRef.Namespace}
