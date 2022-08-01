3dc657c12f61434984729a5e878c21615e81b9c1
ThibaudAv <thibaud.avenier@gmail.com>
2022-08-01 18:03:57 +0200

    test: create api definition with management context

--- controllers/apidefinition_controller_test.go
+++ controllers/apidefinition_controller_test.go
@@ -27,8 +27,10 @@ import (
 	. "github.com/onsi/gomega"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes/scheme"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	gio "github.com/gravitee-io/gravitee-kubernetes-operator/api/v1alpha1"
+	"github.com/gravitee-io/gravitee-kubernetes-operator/apim"
 )
 
 // +kubebuilder:docs-gen:collapse=Imports
@@ -59,6 +61,24 @@ var _ = Describe("API Definition Controller", func() {
 		return false, nil
 	}
 
+	AfterEach(func() {
+		ctx := context.Background()
+
+		// Delete the API definition
+		err := k8sClient.DeleteAllOf(ctx, new(gio.ApiDefinition), &client.DeleteAllOfOptions{
+			ListOptions:   client.ListOptions{Namespace: namespace},
+			DeleteOptions: client.DeleteOptions{},
+		})
+		Expect(err).ToNot(HaveOccurred())
+
+		// Delete the ManagementContext
+		err = k8sClient.DeleteAllOf(ctx, new(gio.ManagementContext), &client.DeleteAllOfOptions{
+			ListOptions:   client.ListOptions{Namespace: namespace},
+			DeleteOptions: client.DeleteOptions{},
+		})
+		Expect(err).ToNot(HaveOccurred())
+	})
+
 	Context("API definition Resource", func() {
 		It("Should create an API Definition", func() {
 			By("Without a management context")
@@ -80,7 +100,6 @@ var _ = Describe("API Definition Controller", func() {
 
 			api.Namespace = namespace
 
-			Expect(err).ToNot(HaveOccurred())
 			Expect(k8sClient.Create(ctx, api)).Should(Succeed())
 
 			apiLookupKey := types.NamespacedName{Name: api.Name, Namespace: namespace}
@@ -93,12 +112,71 @@ var _ = Describe("API Definition Controller", func() {
 
 			Expect(createdApi.Spec.Name).Should(Equal(apiName))
 
-			Expect(k8sClient.Delete(ctx, api)).Should(Succeed())
+			res, err := cli.Get(apiUrl)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(res.StatusCode).To(Equal(200))
+		})
 
+		It("Should create an API Definition", func() {
+			By("With a management context")
+
+			const apimCtxSample = "../config/samples/context/dev/managementcontext_credentials.yaml"
+			const apiSample = "../config/samples/apim/basic-example-with-ctx.yml"
+			const apiName = "K8s Basic Example With Management Context"
+			const apiUrl = "http://localhost:9000/gateway/k8s-basic-with-ctx"
+
+			ctx := context.Background()
+
+			// Create the management context
+			crdMgmtContext, err := ioutil.ReadFile(apimCtxSample)
+			Expect(err).ToNot(HaveOccurred())
+
+			decodedMgmtContext, _, err := decode(crdMgmtContext, &gvk, new(gio.ManagementContext))
+			Expect(err).ToNot(HaveOccurred())
+
+			mgmtContext, ok := decodedMgmtContext.(*gio.ManagementContext)
+			Expect(ok).To(BeTrue())
+
+			mgmtContext.Namespace = namespace
+			Expect(k8sClient.Create(ctx, mgmtContext)).Should(Succeed())
+
+			// Create the API definition
+			crdApiDefinition, err := ioutil.ReadFile(apiSample)
+			Expect(err).ToNot(HaveOccurred())
+
+			decodedApiDefinition, _, err := decode(crdApiDefinition, &gvk, new(gio.ApiDefinition))
+			Expect(err).ToNot(HaveOccurred())
+
+			apiDefinition, ok := decodedApiDefinition.(*gio.ApiDefinition)
+			Expect(ok).To(BeTrue())
+
+			apiDefinition.Namespace = namespace
+
+			Expect(k8sClient.Create(ctx, apiDefinition)).Should(Succeed())
+
+			// Check the API is available in k8s
+			apiLookupKey := types.NamespacedName{Name: apiDefinition.Name, Namespace: namespace}
+			createdApi := new(gio.ApiDefinition)
+
+			Eventually(func() bool {
+				err = k8sClient.Get(ctx, apiLookupKey, createdApi)
+				return err == nil
+			}, timeout, interval).Should(BeTrue())
+
+			Expect(createdApi.Spec.Name).Should(Equal(apiName))
+
+			// Call gateway to check the API is available
 			res, err := cli.Get(apiUrl)
 			Expect(err).ToNot(HaveOccurred())
 			Expect(res.StatusCode).To(Equal(200))
 
+			// Call apim rest api to check the API is available
+			httpClient := http.Client{Timeout: 5 * time.Second}
+			apimClient := apim.NewClient(ctx, mgmtContext, httpClient)
+
+			apis, err := apimClient.FindByCrossId(createdApi.Status.ApiID)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(len(apis)).To(Equal(1))
 		})
 	})
 })
