156f43d97e611a7a46e4394fe39f0d339e98b73f
ThibaudAv <thibaud.avenier@gmail.com>
2022-08-01 15:18:26 +0200

    refactor: use apim_client to interact with apim rest API

--- delegates/apis/import.go
+++ delegates/apis/import.go
@@ -1,10 +1,7 @@
 package apis
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 
 	"github.com/gravitee-io/gravitee-kubernetes-operator/pkg/keys"
@@ -18,7 +15,6 @@ import (
 
 func (d *Delegate) importToManagementApi(
 	api *gio.ApiDefinition,
-	apimCtx *gio.ManagementContext,
 	apiJson []byte,
 ) error {
 	apiId := api.Status.ApiID
@@ -26,82 +22,34 @@ func (d *Delegate) importToManagementApi(
 
 	log := d.log.WithValues("apiId", apiId).WithValues("api.name", apiName, "api.crossId", apiId)
 
-	if apimCtx == nil {
+	if d.apimClient == nil {
 		log.Info("No management context associated to the API, skipping import to Management API")
 		return nil
 	}
 
-	findApiResp, findApiErr := d.findByCrossId(apimCtx, apiId)
+	apis, findApiErr := d.apimClient.FindByCrossId(apiId)
 
 	if findApiErr != nil {
 		return findApiErr
 	}
 
-	if findApiResp.Body != nil {
-		defer findApiResp.Body.Close()
-	}
-
-	if findApiResp.StatusCode != http.StatusOK {
-		// TODO parse response body as a map and log
-		return fmt.Errorf("an error as occured trying to find API %s, HTTP Status: %d ", apiId, findApiResp.StatusCode)
-	}
-
-	body, readErr := ioutil.ReadAll(findApiResp.Body)
-	if readErr != nil {
-		log.Error(readErr, "Unable to read apis response body")
-	}
-
 	// If the API does not exist (ie. 404) it should be a POST
 	importHttpMethod := http.MethodPut
-	var result []interface{}
 
-	err := json.Unmarshal(body, &result)
-	if err != nil {
-		log.Error(err, "Unable to marshal API definition")
-		return err
-	}
-
-	if len(result) == 0 {
+	if len(apis) == 0 {
 		log.Info("No match found for API, switching to creation mode", "crossId", apiId)
 		importHttpMethod = http.MethodPost
 	}
 
-	importResp, importErr := d.doImport(importHttpMethod, apimCtx, apiJson)
+	importErr := d.apimClient.Import(importHttpMethod, apiJson)
 
 	if importErr != nil {
 		log.Error(importErr, "Unable to import the api into the Management API")
 		return importErr
 	}
 
-	if importResp.Body != nil {
-		defer importResp.Body.Close()
-	}
-
-	if importResp.StatusCode < 200 || importResp.StatusCode > 299 {
-		log.Error(nil, "Unable to import the api into the Management API")
-		return fmt.Errorf("management has returned a %d code", importResp.StatusCode)
-	}
-
 	log.Info("Api has been pushed to the Management API")
 	return nil
-}
-
-func (d *Delegate) doImport(
-	importHttpMethod string,
-	apimCtx *gio.ManagementContext,
-	apiJson []byte,
-) (*http.Response, error) {
-	url := apimCtx.Spec.BuildUrl("/apis/import?definitionVersion=2.0.0")
-	req, err := http.NewRequestWithContext(d.ctx, importHttpMethod, url, bytes.NewBuffer(apiJson))
-
-	if err != nil {
-		return nil, fmt.Errorf("unable to import the api into the Management API")
-	}
-
-	req.Header.Add("Content-Type", "application/json")
-	apimCtx.Spec.Authenticate(req)
-	resp, err := d.http.Do(req)
-	return resp, err
 }
 
 // This function is applied to all ingresses which are using the ApiDefinition template
