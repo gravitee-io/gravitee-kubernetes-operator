64122e411beea8860474de754f8b468f055756be
ThibaudAv <thibaud.avenier@gmail.com>
2022-08-02 12:37:49 +0200

    refactor: extract config_map update in dedicated file

--- delegates/apis/create.go
+++ delegates/apis/create.go
@@ -5,11 +5,6 @@ import (
 
 	"github.com/gravitee-io/gravitee-kubernetes-operator/api/model"
 	gio "github.com/gravitee-io/gravitee-kubernetes-operator/api/v1alpha1"
-
-	"github.com/gravitee-io/gravitee-kubernetes-operator/pkg/keys"
-	v1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/types"
 )
 
 const (
@@ -81,48 +76,4 @@ func (d *Delegate) addPlan(api *gio.ApiDefinition) {
 			},
 		}
 	}
-}
-
-func (d *Delegate) updateConfigMap(
-	api *gio.ApiDefinition,
-	apiJson []byte,
-) (bool, error) {
-	// Create configmap with some specific metadata that will be used to check changes across 'Update' events.
-	cm := &v1.ConfigMap{}
-
-	cm.Namespace = api.Namespace
-	cm.Name = api.Name
-	cm.CreationTimestamp = metav1.Now()
-	cm.Labels = map[string]string{
-		"managed-by": keys.CrdGroup,
-		"gio-type":   keys.CrdApiDefinitionResource + "." + keys.CrdGroup,
-	}
-
-	cm.Data = map[string]string{
-		"definition":        string(apiJson),
-		"definitionVersion": api.ResourceVersion,
-	}
-
-	if d.apimCtx != nil {
-		cm.Data["organizationId"] = d.apimCtx.Spec.OrgId
-		cm.Data["environmentId"] = d.apimCtx.Spec.EnvId
-	}
-
-	currentapiDefinition := &v1.ConfigMap{}
-	err := d.cli.Get(d.ctx, types.NamespacedName{Name: cm.Name, Namespace: cm.Namespace}, currentapiDefinition)
-
-	if err == nil {
-		if currentapiDefinition.Data["definitionVersion"] != api.ResourceVersion {
-			d.log.Info("Updating ConfigMap", "id", api.Spec.Id)
-			// Only update the confimap if resource version has changed (means api definition has changed).
-			err = d.cli.Update(d.ctx, cm)
-		} else {
-			d.log.Info("No change detected on api. Skipped.", "id", api.Spec.Id)
-			return false, nil
-		}
-	} else {
-		d.log.Info("Creating configmap for api.", "id", api.Spec.Id, "name", api.Name)
-		err = d.cli.Create(d.ctx, cm)
-	}
-	return true, err
 }
