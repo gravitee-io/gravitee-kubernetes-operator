8811eea71df9298256ae0b53b1ee5fda6927564d
ThibaudAv <thibaud.avenier@gmail.com>
2022-08-12 15:47:58 +0200

    feat: send delete request to management api when apiDefinition are deleting

--- internal/apim/apis/delete.go
+++ internal/apim/apis/delete.go
@@ -2,13 +2,29 @@ package apis
 
 import (
 	gio "github.com/gravitee-io/gravitee-kubernetes-operator/api/v1alpha1"
+	"github.com/gravitee-io/gravitee-kubernetes-operator/pkg/keys"
+	util "sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 )
 
 func (d *Delegate) delete(
-	api *gio.ApiDefinition,
+	apiDefinition *gio.ApiDefinition,
 ) error {
-	d.log.Info("Deleting API Definition")
-	err := d.deleteConfigMap(api.Namespace, api.Name)
+	// Do nothing if finalizer is already removed
+	if !util.ContainsFinalizer(apiDefinition, keys.ApiDefinitionDeletionFinalizer) {
+		return nil
+	}
 
-	return err
+	if d.apimClient != nil {
+		d.log.Info("Delete API definition into Management API")
+		err := d.apimClient.DeleteApi(apiDefinition.Status.ID)
+		if err != nil {
+			d.log.Error(err, "Unable to delete API definition into Management API")
+			return err
+		}
+	}
+
+	// Remove finalizer when API definition is fully deleted
+	util.RemoveFinalizer(apiDefinition, keys.ApiDefinitionDeletionFinalizer)
+
+	return d.k8sClient.Update(d.ctx, apiDefinition)
 }
