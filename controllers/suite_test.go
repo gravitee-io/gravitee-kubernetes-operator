158f4fa7dcb546ca69db7ab4002e60fc1c387474
ThibaudAv <thibaud.avenier@gmail.com>
2022-08-17 12:24:40 +0200

    chore: migrate to ginkgo v2

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -20,12 +20,11 @@ import (
 	"testing"
 	"time"
 
-	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"k8s.io/client-go/kubernetes/scheme"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/envtest/printer"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
@@ -50,12 +49,10 @@ const (
 func TestAPIs(t *testing.T) {
 	RegisterFailHandler(Fail)
 
-	RunSpecsWithDefaultAndCustomReporters(t,
-		"GKO Controllers Suite",
-		[]Reporter{printer.NewlineReporter{}})
+	RunSpecs(t, "GKO Controllers Suite")
 }
 
-var _ = BeforeSuite(func(done Done) {
+var _ = BeforeSuite(func() {
 	logf.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)))
 
 	err := gio.AddToScheme(scheme.Scheme)
@@ -88,9 +85,7 @@ var _ = BeforeSuite(func(done Done) {
 
 	Expect(k8sClient).ToNot(BeNil())
 
-	close(done)
-
-}, 60)
+})
 
 var _ = AfterSuite(func() {
 	By("tearing down the test environment")
