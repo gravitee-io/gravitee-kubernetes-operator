d2ce06cdaa4d1786398a2dfa76f95685ff1d87b1
ThibaudAv <thibaud.avenier@gmail.com>
2022-08-30 18:41:45 +0200

    refactor: improve rbac order

--- controllers/apim/apidefinition/apidefinition_controller.go
+++ controllers/apim/apidefinition/apidefinition_controller.go
@@ -46,11 +46,11 @@ type Reconciler struct {
 	Recorder record.EventRecorder
 }
 
+//+kubebuilder:rbac:groups="",resources=secrets,verbs=get
+//+kubebuilder:rbac:groups="",resources=configmaps,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=gravitee.io,resources=apidefinitions,verbs=get;list;watch;create;update;patch;delete;deletecollection
 //+kubebuilder:rbac:groups=gravitee.io,resources=apidefinitions/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups="",resources=secrets,verbs=get
 //+kubebuilder:rbac:groups=gravitee.io,resources=apidefinitions/finalizers,verbs=update
-//+kubebuilder:rbac:groups="",resources=configmaps,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
