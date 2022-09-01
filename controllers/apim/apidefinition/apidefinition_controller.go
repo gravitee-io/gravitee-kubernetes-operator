1a61aacba597a44560f823d4ce56fdc381dae8bb
Antoine CORDIER <antoine.cordier@graviteesource.com>
2022-09-01 16:24:48 +0200

    fix: allow manager to publish events

--- controllers/apim/apidefinition/apidefinition_controller.go
+++ controllers/apim/apidefinition/apidefinition_controller.go
@@ -52,6 +52,7 @@ type Reconciler struct {
 //+kubebuilder:rbac:groups=gravitee.io,resources=apidefinitions,verbs=get;list;watch;create;update;patch;delete;deletecollection
 //+kubebuilder:rbac:groups=gravitee.io,resources=apidefinitions/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=gravitee.io,resources=apidefinitions/finalizers,verbs=update
+//+kubebuilder:rbac:groups="",resources=events,verbs=create;patch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
