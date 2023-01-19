b18bc7df616822f9086928929df79407521ab08c
Antoine CORDIER <antoine.cordier@graviteesource.com>
2023-01-19 18:07:30 +0100

    ci: clean makefile

---
+++ make/build.mk
@@ -0,0 +1,14 @@
+##@ 🔨Build
+
+.PHONY: build
+build: generate ## Build manager binary.
+	go build -o bin/manager main.go
+
+.PHONY: manifests
+manifests: controller-gen ## Generate WebhookConfiguration, ClusterRole and CustomResourceDefinition objects.
+	$(CONTROLLER_GEN) crd webhook paths="./..." output:crd:artifacts:config=config/crd/bases
+
+.PHONY: generate
+generate: controller-gen ## Generate code containing DeepCopy, DeepCopyInto, and DeepCopyObject method implementations.
+	$(CONTROLLER_GEN) object:headerFile="hack/boilerplate.go.txt" paths="./..."
+
