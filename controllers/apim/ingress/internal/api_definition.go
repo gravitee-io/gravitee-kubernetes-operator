e47f3587188805985006e52ccabda090d02dbf7c
Okhelifi <ouahid.khelifi@gmail.com>
2023-02-07 10:27:43 +0100

    refactor: extract ingress controller and api definition methods

---
+++ controllers/apim/ingress/internal/api_definition.go
@@ -0,0 +1,90 @@
+// Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//         http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package internal
+
+import (
+	"fmt"
+
+	"github.com/go-logr/logr"
+	"github.com/gravitee-io/gravitee-kubernetes-operator/api/model"
+	"github.com/gravitee-io/gravitee-kubernetes-operator/api/v1alpha1"
+	netV1 "k8s.io/api/networking/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	util "sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+)
+
+// MergeApiDefinition
+// Transform the ingress as an API Definition as per https://kubernetes.io/docs/concepts/services-networking/ingress/#the-ingress-resource
+func MergeApiDefinition(
+	log logr.Logger,
+	apiDefinition *v1alpha1.ApiDefinition,
+	ingress *netV1.Ingress,
+) *v1alpha1.ApiDefinition {
+	api := &v1alpha1.ApiDefinition{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      ingress.Name,
+			Namespace: ingress.Namespace,
+		},
+		Spec: *apiDefinition.Spec.DeepCopy(),
+	}
+
+	log.Info("Merge Ingress with API Definition")
+
+	for _, rule := range ingress.Spec.Rules {
+		for _, path := range rule.HTTP.Paths {
+			service := path.Backend.Service
+
+			//TODO: How-to dedal with PathType ?
+			api.Spec.Proxy = &model.Proxy{
+				VirtualHosts: []*model.VirtualHost{
+					{
+						Path: path.Path,
+					},
+				},
+				Groups: []*model.EndpointGroup{
+					{
+						Name: "default",
+						Endpoints: []*model.HttpEndpoint{
+							{
+								Name:   service.Name,
+								Target: fmt.Sprintf("http://%s.%s.svc.cluster.local:%d", service.Name, ingress.Namespace, service.Port.Number),
+							},
+						},
+					},
+				},
+			}
+		}
+	}
+	return api
+}
+
+func (d *Delegate) CreateOrUpdateApiDefintion(ingress *netV1.Ingress, namespace string) (util.OperationResult, error) {
+	apiDefinition, err := d.ResolveApiDefinition(ingress, namespace)
+	if err != nil {
+		return util.OperationResultNone, err
+	}
+
+	api := &v1alpha1.ApiDefinition{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      apiDefinition.Name,
+			Namespace: apiDefinition.Namespace,
+		},
+	}
+
+	return util.CreateOrUpdate(d.ctx, d.k8s, api, func() error {
+		apiDefinition.Spec.DeepCopyInto(&api.Spec)
+		return util.SetOwnerReference(ingress, api, d.k8s.Scheme())
+	})
+}
