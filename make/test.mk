1a607dea67a14d2e76fe2c13965949131562cda1
Antoine CORDIER <antoine.cordier@graviteesource.com>
2023-02-10 09:14:51 +0000

    test: get integration coverage

--- make/test.mk
+++ make/test.mk
@@ -1,7 +1,9 @@
 ##@ Test
 
 GOTESTARGS ?= ""
+COVERPKG = "github.com/gravitee-io/gravitee-kubernetes-operator/..."
+TIMEOUT = 380s 
 .PHONY: test
 test: manifests generate install ginkgo ## Run tests.
 	kubectl config use-context k3d-graviteeio
-	KUBEBUILDER_ASSETS=USE_EXISTING_CLUSTER=true $(GINKGO) $(GOTESTARGS) --timeout 380s --cover --coverprofile=cover.out ./...
+	KUBEBUILDER_ASSETS=USE_EXISTING_CLUSTER=true $(GINKGO) $(GOTESTARGS) --timeout $(TIMEOUT) --cover --coverprofile=cover.out --coverpkg=$(COVERPKG)  ./...
