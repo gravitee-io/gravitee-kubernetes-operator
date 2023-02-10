9255fcac4e22b8fbdd37b977f30f8955132a802e
Antoine CORDIER <antoine.cordier@graviteesource.com>
2023-02-10 19:15:57 +0000

    ci: add helm-docs targets

--- make/helm.mk
+++ make/helm.mk
@@ -10,6 +10,9 @@ helm-prepare: manifests kustomize ## prepare helm chart from kustomize resources
 	npx zx scripts/helm-transform.mjs
 
 .PHONY: helm-template
-helm-template: manifests kustomize helm-prepare ## generates a templated bundle.yml
+helm-template: manifests kustomize helm-prepare ## generates a templated (legacy) bundle.yml
 	helm template --include-crds  helm/gko -n gko-system > bundle.yml
 
+.PHONY: helm-document ## generates helm chart documentation
+helm-document: helm-docs
+	$(HELMDOCS) --dry-run
