2d517564efcde0de810515b79c9f94abc1c80694
Antoine CORDIER <antoine.cordier@graviteesource.com>
2023-02-11 18:04:47 +0000

    ci: update chart and chart reference during release

--- make/lint.mk
+++ make/lint.mk
@@ -3,7 +3,7 @@
 ALL_LINT=lint-commits lint-sources lint-licenses
 
 .PHONY: lint
-lint: lint-commits lint-sources lint-licenses ## Run all lint and fail on error
+lint: lint-commits lint-sources lint-licenses ## Run all linters and fail on error
 
 .PHONY: lint-commits
 lint-commits:  ## Run commitlint and fail on error
@@ -17,9 +17,14 @@ lint-sources: golangci-lint ## Run golangci-lint and fail on error
 	@$(GOLANGCILINT) run ./... 
 
 .PHONY: lint-licenses
-lint-licenses: addlicense ## Run addlicense lint and fail on error
+lint-licenses: addlicense ## Run addlicense linter and fail on error
 	@echo "Checking license headers ..."
 	@$(ADDLICENSE) -check -f LICENSE_TEMPLATE.txt -ignore ".circleci/**" -ignore "config/**" -ignore "helm/crds/**" -ignore ".idea/**" .
+
+.PHONY: add-license
+add-license: addlicense ## Add license headers to files
+	@echo "Adding license headers ..."
+	$(ADDLICENSE) -f LICENSE_TEMPLATE.txt -ignore ".circleci/**" -ignore "config/**" .
 
 .PHONY: clean-tools ## Run all linters
 lint: $(ALL_LINT)
