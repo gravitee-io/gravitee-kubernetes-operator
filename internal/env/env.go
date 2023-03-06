02f4873fe4e2479240b58d0da7bd63e30f9bc337
Antoine CORDIER <antoine.cordier@graviteesource.com>
2023-03-06 18:05:32 +0100

    refactor: store environment config for reuse

---
+++ internal/env/env.go
@@ -0,0 +1,40 @@
+// Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//         http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package env
+
+import (
+	"os"
+)
+
+const (
+	CMTemplate404Name = "TEMPLATE_404_CONFIG_MAP_NAME"
+	CMTemplate404NS   = "TEMPLATE_404_CONFIG_MAP_NAMESPACE"
+	Development       = "DEV_MODE"
+	NS                = "NAMESPACE"
+)
+
+var Config = struct {
+	NS                string
+	Development       bool
+	CMTemplate404Name string
+	CMTemplate404NS   string
+}{}
+
+func init() {
+	Config.NS = os.Getenv(NS)
+	Config.Development = os.Getenv(Development) == "true"
+	Config.CMTemplate404Name = os.Getenv(CMTemplate404Name)
+	Config.CMTemplate404NS = os.Getenv(CMTemplate404NS)
+}
