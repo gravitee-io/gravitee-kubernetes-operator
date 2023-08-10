f0345fa53b0a9d04e05e8a4e276deb27ac01867a
Antoine CORDIER <antoine.cordier@graviteesource.com>
2023-08-10 14:58:32 +0200

    feat: allow custom manager image and tag
    see https://gravitee.atlassian.net/browse/APIM-1524

--- make/docker.mk
+++ make/docker.mk
@@ -1,13 +1,27 @@
 ##@ 🐳 Docker
 
 # Image URL to use all building/pushing image targets
-IMG ?= controller:latest
+IMG ?= graviteeio/kubernetes-operator
+TAG ?= latest
+
+# DEV
 
 .PHONY: docker-build
 docker-build: ## Build docker image with the manager.
-	docker build -t ${IMG} .
+	docker build -t ${IMG}:${TAG} .
 
 .PHONY: docker-push
 docker-push: ## Push docker image with the manager.
-	docker push ${IMG}
+	docker push ${IMG}:${TAG}
+
+# RELEASE
+
+.PHONY: docker-build-release
+docker-build-release: ## Build docker image with the manager.
+	docker build -t ${IMG}:${TAG} -t ${IMG}:latest .
+
+.PHONY: docker-push-release
+docker-push-release: ## Push docker image with the manager.
+	docker push  ${IMG}:${TAG}
+	docker push ${IMG}:latest
 
