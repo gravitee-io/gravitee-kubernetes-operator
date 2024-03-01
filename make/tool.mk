2f12efbb4689600e43aca22d34638b4ae78907a7
Kamiel Ahmadpour <kamiel.ahmadpour@graviteesource.com>
2024-03-01 18:24:59 +0000

    chore: update controller-gen & Kustomize dependencies

--- make/tool.mk
+++ make/tool.mk
@@ -17,9 +17,8 @@ ADDLICENSE ?= $(LOCALBIN)/addlicense
 ALL_TOOLS = kustomize controller-gen envtest ginkgo crdoc golangci-lint addlicense helm-unittest
 
 ## Tool Versions
-KUSTOMIZE_VERSION ?= v4.5.7
-CONTROLLER_TOOLS_VERSION ?= v0.13.0
-ENVTEST_K8S_VERSION = 1.24
+KUSTOMIZE_VERSION ?= v5.3.0
+CONTROLLER_TOOLS_VERSION ?= v0.14.0
 
 KUSTOMIZE_INSTALL_SCRIPT ?= "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"
 .PHONY: kustomize
