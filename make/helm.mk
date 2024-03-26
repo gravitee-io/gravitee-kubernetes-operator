39981d2d9e5399664c077a7faa646752989f0dc9
Antoine CORDIER <antoine.cordier@graviteesource.com>
2024-03-26 15:42:09 +0000

    ci: clean up tool chain

--- make/helm.mk
+++ make/helm.mk
@@ -1,7 +1,5 @@
 ##@ ☸ Helm
 
-KUSTOMIZE_ARGS ?= ""
-
 .PHONY: helm-template
-helm-template: manifests kustomize ## Generates legacy bundle.yml file from helm chart
+helm-template: manifests ## Generates legacy bundle.yml file from helm chart
 	helm template --include-crds  helm/gko -n gko-system > bundle.yml
