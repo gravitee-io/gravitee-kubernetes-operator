82ad5d8acae2e5a8e850a3e665b9379b18599e47
Antoine CORDIER <antoine.cordier@graviteesource.com>
2024-03-26 15:42:09 +0000

    ci: clean up tool chain

--- make/helm.mk
+++ make/helm.mk
@@ -1,14 +1,5 @@
 ##@ ☸ Helm
 
-KUSTOMIZE_ARGS ?= ""
-
-
-.PHONY:helm-prepare
-helm-prepare: manifests kustomize ## Prepare helm chart from kustomize resources
-	$(KUSTOMIZE) build config/default -o helm/gko/templates/bundle.yaml 
-	npx zx scripts/helm-transform.mjs
-	$(MAKE) add-license
-
 .PHONY: helm-template
-helm-template: manifests kustomize ## Generates legacy bundle.yml file from helm chart
+helm-template: manifests ## Generates legacy bundle.yml file from helm chart
 	helm template --include-crds  helm/gko -n gko-system > bundle.yml
