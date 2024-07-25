ac8cf0b86f9066529eae4918446ab656e0c6d715
Antoine CORDIER <antoine.cordier@graviteesource.com>
2024-07-25 12:03:42 +0200

    ci: enable remote debug in helm
    (cherry picked from commit c727c047f9a2c5e5ca1c2590fe130ce0ecdb625a)

--- make/docker.mk
+++ make/docker.mk
@@ -15,7 +15,6 @@ docker-push: ## Push docker image with the manager.
 	docker push ${IMG}:${TAG}
 
 # RELEASE
-
 .PHONY: docker-build-release
 docker-build-release: ## Build docker image with the manager.
 	docker build -t ${IMG}:${TAG} -t ${IMG}:latest .
@@ -25,3 +24,5 @@ docker-push-release: ## Push docker image with the manager.
 	docker push  ${IMG}:${TAG}
 	docker push ${IMG}:latest
 
+docker-build-debug: ## Build docker image with remote debug enabled
+	docker build -f Dockerfile.debug -t ${IMG}:${TAG} .
