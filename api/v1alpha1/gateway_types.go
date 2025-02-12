617b189246d47677c51989714c165edd50c11b33
Antoine CORDIER <antoine.cordier@graviteesource.com>
2025-02-12 14:32:23 +0000

    feat: add controller for gravitee gateways

---
+++ api/v1alpha1/gateway_types.go
@@ -0,0 +1,50 @@
+// Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//         http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1alpha1
+
+import (
+	"github.com/gravitee-io/gravitee-kubernetes-operator/api/model/gateway"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+// +kubebuilder:storageversion
+type GraviteeGateway struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   GraviteeGatewaySpec   `json:"spec,omitempty"`
+	Status GraviteeGatewayStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+type GraviteeGatewayList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []GraviteeGateway `json:"items"`
+}
+
+// +kubebuilder:object:generate=true
+type GraviteeGatewaySpec struct {
+	*gateway.GraviteeGateway `json:",inline"`
+}
+
+type GraviteeGatewayStatus struct {
+}
+
+func init() {
+	SchemeBuilder.Register(&GraviteeGateway{}, &GraviteeGatewayList{})
+}
--- test/internal/integration/manager/manager.go
+++ test/internal/integration/manager/manager.go
@@ -19,6 +19,7 @@ import (
 	"os"
 
 	policygroups "github.com/gravitee-io/gravitee-kubernetes-operator/controllers/apim/policygroups"
+	"github.com/gravitee-io/gravitee-kubernetes-operator/controllers/gateway-api/graviteegw"
 
 	v1 "k8s.io/api/core/v1"
 	"sigs.k8s.io/controller-runtime/pkg/cache"
@@ -138,6 +139,13 @@ func init() {
 			Scheme:   mgr.GetScheme(),
 			Recorder: mgr.GetEventRecorderFor("ingress-controller"),
 			Watcher:  watch.New(context.Background(), Client(), &netV1.IngressList{}),
+		}).SetupWithManager(mgr),
+	)
+
+	runtimeUtil.Must(
+		(&graviteegw.Reconciler{
+			Scheme:   mgr.GetScheme(),
+			Recorder: mgr.GetEventRecorderFor("gravitee-gateway-controller"),
 		}).SetupWithManager(mgr),
 	)
 
