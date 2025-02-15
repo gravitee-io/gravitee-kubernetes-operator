e0612e64db7c0cd87f2c32dcf35398019fa07562
Antoine CORDIER <antoine.cordier@graviteesource.com>
2025-02-15 17:02:47 +0000

    fix: use safe method when updating secrets
    (cherry picked from commit 06738578b194aae4615cbbcc02c69ea51dc6c22e)
    (cherry picked from commit 5b713cb195f03b942cb64fc81a949b80be0b7716)

--- internal/template/compiler.go
+++ internal/template/compiler.go
@@ -180,7 +180,7 @@ func addFinalizer(ctx context.Context, obj client.Object) error {
 
 		util.AddFinalizer(object, core.TemplatingFinalizer)
 
-		return cli.Update(ctx, object)
+		return k8s.UpdateSafely(ctx, object)
 	}
 
 	return nil
