d2c8b7b7fc59e48e319b74b5cbda9cd098336395
Antoine CORDIER <antoine.cordier@graviteesource.com>
2025-06-23 19:25:27 +0200

    refactor: implement our own create or update

--- internal/k8s/ctrl.go
+++ internal/k8s/ctrl.go
@@ -16,9 +16,11 @@ package k8s
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"github.com/gravitee-io/gravitee-kubernetes-operator/internal/log"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	util "sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
@@ -32,15 +34,41 @@ func RequeueError(ctx context.Context, err error, obj client.Object) (ctrl.Resul
 }
 
 func CreateOrUpdate(ctx context.Context, obj client.Object, fns ...util.MutateFn) error {
-	if _, err := util.CreateOrUpdate(ctx, GetClient(), obj, func() error {
+	key := client.ObjectKeyFromObject(obj)
+	if err := GetClient().Get(ctx, key, obj); err != nil {
+		if !apierrors.IsNotFound(err) {
+			return err
+		}
 		for _, f := range fns {
-			if e := f(); e != nil {
-				return e
+			if err := mutate(f, key, obj); err != nil {
+				return err
 			}
 		}
+
+		if err := GetClient().Create(ctx, obj); err != nil {
+			return err
+		}
 		return nil
-	}); err != nil {
+	}
+
+	for _, f := range fns {
+		if err := mutate(f, key, obj); err != nil {
+			return err
+		}
+	}
+
+	if err := Update(ctx, obj); err != nil {
 		return err
+	}
+	return nil
+}
+
+func mutate(f util.MutateFn, key client.ObjectKey, obj client.Object) error {
+	if err := f(); err != nil {
+		return err
+	}
+	if newKey := client.ObjectKeyFromObject(obj); key != newKey {
+		return fmt.Errorf("MutateFn cannot mutate object name and/or object namespace")
 	}
 	return nil
 }
