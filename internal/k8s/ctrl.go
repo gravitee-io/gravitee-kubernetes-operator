90bd73e7f95e0f54ee88600230ecb017f59287d9
Antoine CORDIER <antoine.cordier@graviteesource.com>
2025-06-24 19:05:30 +0200

    refactor: revert implement our own create or update
    This reverts commit 4f7a54ffd1fa54f669dce6c29ff71c049fe12e32.

--- internal/k8s/ctrl.go
+++ internal/k8s/ctrl.go
@@ -16,10 +16,8 @@ package k8s
 
 import (
 	"context"
-	"fmt"
 	"time"
 
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	util "sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
@@ -32,41 +30,15 @@ func RequeueError(err error) (ctrl.Result, error) {
 }
 
 func CreateOrUpdate(ctx context.Context, obj client.Object, fns ...util.MutateFn) error {
-	key := client.ObjectKeyFromObject(obj)
-	if err := GetClient().Get(ctx, key, obj); err != nil {
-		if !apierrors.IsNotFound(err) {
-			return err
-		}
+	if _, err := util.CreateOrUpdate(ctx, GetClient(), obj, func() error {
 		for _, f := range fns {
-			if err := mutate(f, key, obj); err != nil {
-				return err
+			if e := f(); e != nil {
+				return e
 			}
 		}
-
-		if err := GetClient().Create(ctx, obj); err != nil {
-			return err
-		}
 		return nil
-	}
-
-	for _, f := range fns {
-		if err := mutate(f, key, obj); err != nil {
-			return err
-		}
-	}
-
-	if err := Update(ctx, obj); err != nil {
+	}); err != nil {
 		return err
-	}
-	return nil
-}
-
-func mutate(f util.MutateFn, key client.ObjectKey, obj client.Object) error {
-	if err := f(); err != nil {
-		return err
-	}
-	if newKey := client.ObjectKeyFromObject(obj); key != newKey {
-		return fmt.Errorf("MutateFn cannot mutate object name and/or object namespace")
 	}
 	return nil
 }
