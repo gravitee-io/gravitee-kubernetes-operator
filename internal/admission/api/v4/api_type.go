a26d2f86097800d91b55774a63a8022ac99d98b6
Kamiel Ahmadpour <kamiel.ahmadpour@graviteesource.com>
2025-09-23 12:20:07 +0100

    fix: make API type immutable
    see: https://gravitee.atlassian.net/browse/GKO-1528
    (cherry picked from commit 42c53788a259bee5462dd20544cabc641916036a)
    
    # Conflicts:
    #	internal/admission/api/v4/api_type.go
    #	test/integration/admission/api/v4/update_withoutContext_withInvalidAPIType.go

---
+++ internal/admission/api/v4/api_type.go
@@ -0,0 +1,29 @@
+// Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//         http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v4
+
+import (
+	"github.com/gravitee-io/gravitee-kubernetes-operator/internal/core"
+	"github.com/gravitee-io/gravitee-kubernetes-operator/internal/errors"
+)
+
+func validateApiType(oldApi, newAPI core.ApiDefinitionObject) *errors.AdmissionError {
+	if newAPI.GetType() != oldApi.GetType() {
+		return errors.NewSeveref("it is not possible to change API Type. Old type '%s', "+
+			"new type '%s'", oldApi.GetType(), newAPI.GetType())
+	}
+
+	return nil
+}
