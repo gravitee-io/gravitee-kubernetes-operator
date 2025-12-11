a1d7b96c2c7e274dd36501f112cbf727e8664b88
a-cordier <antoine.cordier@graviteesource.com>
2025-12-11 18:14:21 +0000

    fix: watch gateways in http route controller

--- controllers/gateway-api/httproute/internal/watch.go
+++ controllers/gateway-api/httproute/internal/watch.go
@@ -18,6 +18,7 @@ import (
 	"context"
 
 	"github.com/gravitee-io/gravitee-kubernetes-operator/internal/k8s"
+	"github.com/gravitee-io/gravitee-kubernetes-operator/internal/log"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
@@ -55,4 +56,51 @@ func buildRequest(route gwAPIv1.HTTPRoute) reconcile.Request {
 			Name:      route.Name,
 		},
 	}
+}
+
+func WatchGateways() handler.EventHandler {
+	return handler.EnqueueRequestsFromMapFunc(requestsFromGateway)
+}
+
+func requestsFromGateway(ctx context.Context, obj client.Object) []reconcile.Request {
+	gw, ok := obj.(*gwAPIv1.Gateway)
+	if !ok {
+		return nil
+	}
+
+	listOpts := &client.ListOptions{}
+	list := &gwAPIv1.HTTPRouteList{}
+	if err := k8s.GetClient().List(ctx, list, listOpts); err != nil {
+		log.Error(ctx, err, "failed to list HTTPRoutes when watching gateway")
+		return []reconcile.Request{}
+	}
+
+	var reqs []reconcile.Request
+	for _, route := range list.Items {
+		if referencesGateway(&route, gw) {
+			reqs = append(reqs, buildRequest(route))
+		}
+	}
+
+	return reqs
+}
+
+func referencesGateway(route *gwAPIv1.HTTPRoute, gw *gwAPIv1.Gateway) bool {
+	for _, ref := range route.Spec.ParentRefs {
+		if !k8s.IsGatewayKind(ref) {
+			continue
+		}
+		if string(ref.Name) != gw.Name {
+			continue
+		}
+		refNS := ref.Namespace
+		if refNS == nil {
+			if route.Namespace == gw.Namespace {
+				return true
+			}
+		} else if string(*refNS) == gw.Namespace {
+			return true
+		}
+	}
+	return false
 }
