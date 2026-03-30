a0874d13e97fbc3a2f5de4ee3dec74862b34f339
Kamiel Ahmadpour <kamiel.ahmadpour@graviteesource.com>
2026-03-30 13:46:32 +0100

    fix: let users pass page HRID for plan general condition
    see https://gravitee.atlassian.net/browse/GKO-2703
    
    (cherry picked from commit 012042580ee879caac8cbfe7c4870dfd6690e42c)
    
    # Conflicts:
    #	api/v1alpha1/apiv4definition_types.go

--- api/v1alpha1/apiv4definition_types.go
+++ api/v1alpha1/apiv4definition_types.go
@@ -19,6 +19,8 @@ package v1alpha1
 import (
 	"fmt"
 
+	"k8s.io/utils/ptr"
+
 	"github.com/gravitee-io/gravitee-kubernetes-operator/api/model/api/base"
 
 	"github.com/gravitee-io/gravitee-kubernetes-operator/internal/hash"
@@ -158,6 +160,13 @@ func (api *ApiV4Definition) pickPlanIDs(mCtx core.ContextModel) *map[string]*v4.
 		} else if plan.ID == "" {
 			p.ID = uuid.FromStrings(api.pickID(mCtx), key)
 		}
+		// This if condition can be removed once GKO moved to using the new Automation APIs
+		if p.GeneralConditions != nil && api.Spec.Pages != nil {
+			if page, ok := (*api.Spec.Pages)[*p.GeneralConditions]; ok {
+				p.GeneralConditions = ptr.To(page.ID)
+			}
+		}
+
 		plans[key] = p
 	}
 	return &plans
