0ee17453e6c7831fac1e6bc2aa15a9625caabaeb
a-cordier <antoine.cordier@graviteesource.com>
2026-07-22 14:27:16 +0200

    ci: migrate binaries to go tool

--- hack/make/test.mk
+++ hack/make/test.mk
@@ -10,13 +10,13 @@ IT_ARGS ?= ""
 TIMEOUT ?= 1200s 
 
 .PHONY: it
-it: use-cluster install install-go-tools ## Run integration tests
-	$(GINKGO) $(IT_ARGS) --timeout $(TIMEOUT)  test/integration/...
+it: use-cluster install ## Run integration tests
+	go tool ginkgo $(IT_ARGS) --timeout $(TIMEOUT)  test/integration/...
 
 UT_ARGS ?= ""
 .PHONY: unit
 unit:  ## Run unit tests
-	$(GINKGO) $(UT_ARGS) test/unit/...
+	go tool ginkgo $(UT_ARGS) test/unit/...
 
 .PHONY: e2e
 e2e:  ## Run all end to end tests
@@ -27,5 +27,5 @@ e2e-focus:  ## Run end to end tests and focus on test having the focus label set
 	$(CHAINSAW) test --config test/e2e/chainsaw/config.yaml --selector focus=true
 
 .PHONY: conformance
-conformance: install-go-tools ## Run end to end tests and focus on test having the focus label set to true
-	$(GOTESTSUM) --format=testname --packages="./test/conformance/kubernetes.io/gateway-api/standard/..." -- -args --gateway-class=gravitee-gateway
+conformance: ## Run conformance tests
+	go tool gotestsum --format=testname --packages="./test/conformance/kubernetes.io/gateway-api/standard/..." -- -args --gateway-class=gravitee-gateway
