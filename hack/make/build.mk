7bc8fceaa0ce6101d51ecba6c7b8c4c1b832c9b5
a-cordier <antoine.cordier@graviteesource.com>
2026-07-22 14:27:16 +0200

    ci: migrate binaries to go tool

--- hack/make/build.mk
+++ hack/make/build.mk
@@ -6,16 +6,16 @@ build: generate ## Build manager binary.
 
 .PHONY: manifests
 manifests: ## Generate CustomResourceDefinition objects.
-	$(CONTROLLER_GEN) crd:maxDescLen=100 paths="./api/..." output:crd:artifacts:config=crds/gravitee.io
+	go tool controller-gen crd:maxDescLen=100 paths="./api/..." output:crd:artifacts:config=crds/gravitee.io
 	@npx zx hack/scripts/annotate-crds.mjs
 	$(MAKE) add-license
 
 .PHONY: manifests-for-docs
 manifests-for-docs: ## Generate CustomResourceDefinition objects.
 	@mkdir -p docs/api/crd
-	$(CONTROLLER_GEN) crd paths="./api/..." output:crd:artifacts:config=docs/api/crd
+	go tool controller-gen crd paths="./api/..." output:crd:artifacts:config=docs/api/crd
 
 .PHONY: generate
 generate: ## Generate code containing DeepCopy, DeepCopyInto, and DeepCopyObject method implementations.
-	$(CONTROLLER_GEN) object:headerFile="hack/license.go.txt" paths="./..."
+	go tool controller-gen object:headerFile="hack/license.go.txt" paths="./..."
 
